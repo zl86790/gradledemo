86fba75098612b2a526bda4abd2efbbf0484510d
lizhe <marshal_li_b@163.com>
2022-03-17 23:36:07 -0700

    init

---
+++ src/main/kotlin/com/lizhe/gradledemo/GradledemoApplication.kt
@@ -0,0 +1,11 @@
+package com.lizhe.gradledemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GradledemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GradledemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/lizhe/gradledemo/GradledemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lizhe.gradledemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GradledemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
